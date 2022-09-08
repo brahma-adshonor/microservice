8e8382b35d016ee9b4fcb13d7a3ed838c537bab8
pei xingxin <xingxinpei@gmail.com>
2022-09-08 15:57:24 +0800

    增加库表和对应model

--- code/app/commodity/model/commodity.go
+++ code/app/commodity/model/commodity.go
@@ -1,4 +1,12 @@
 package model
 
+// 第一个版本，我们只支持未删除的商品，以及上架的商品
 type Model struct {
+	id          int32
+	name        string
+	price       int32 // 单位分
+	pic         string
+	description string
+	sale        int32
+	recommend   int32 // 推荐指数
 }
