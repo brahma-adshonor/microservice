93b3ae9352495c9af33aa0b4d04cdf7b248f0a96
tirozhang <tirozhang@tencent.com>
2022-09-19 15:19:29 +0800

    add testdata file and update sql

---
+++ code/db/db_user/testdata.sql
@@ -0,0 +1,12 @@
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` (`id`,`username`,`password`,`phone`,`email`,`gender`,`status`)VALUES ('1', 'test', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', '18061581849','test@qq.com',1,1);
+INSERT INTO `t_user` (`id`,`username`,`password`,`phone`,`email`,`gender`,`status`)VALUES ('3', 'windy', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG',  '18061581848','test@qq.com',2,1);
+INSERT INTO `t_user` (`id`,`username`,`password`,`phone`,`email`,`gender`,`status`)VALUES ('4', 'zhengsan', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG',  '18061581847','test@qq.com',2,1);
+INSERT INTO `t_user` (`id`,`username`,`password`,`phone`,`email`,`gender`,`status`)VALUES ('5', 'lisi', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG',  '18061581841','test@qq.com',1,1);
+INSERT INTO `t_user` (`id`,`username`,`password`,`phone`,`email`,`gender`,`status`)VALUES ('6', 'wangwu', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG',  '18061581842','test@qq.com',1,1);
+INSERT INTO `t_user` (`id`,`username`,`password`,`phone`,`email`,`gender`,`status`)VALUES ('7', 'lion', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG',  '18061581845','test@qq.com',1,1);
+INSERT INTO `t_user` (`id`,`username`,`password`,`phone`,`email`,`gender`,`status`)VALUES ('8', 'shari', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG',  '18061581844','test@qq.com',1,1);
+INSERT INTO `t_user` (`id`,`username`,`password`,`phone`,`email`,`gender`,`status`)VALUES ('9', 'aewen', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG',  '18061581843','test@qq.com',1,1);
+
