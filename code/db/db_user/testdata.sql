3f5250a55c9749600b042e9426cf6ecdac043a7e
tirozhang <tirozhang@tencent.com>
2022-09-19 17:22:42 +0800

    update password to md5(123456) for test data

--- code/db/db_user/testdata.sql
+++ code/db/db_user/testdata.sql
@@ -2,27 +2,27 @@
 -- Records of t_user
 -- ----------------------------
 INSERT INTO `t_user` (`id`, `username`, `password`, `phone`, `email`, `gender`, `status`)
-VALUES ('1', 'test', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', '18061581849', 'test@qq.com', 1,
+VALUES ('1', 'test', 'e10adc3949ba59abbe56e057f20f883e', '18061581849', 'test@qq.com', 1,
         1);
 INSERT INTO `t_user` (`id`, `username`, `password`, `phone`, `email`, `gender`, `status`)
-VALUES ('3', 'windy', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', '18061581848', 'test@qq.com', 2,
+VALUES ('3', 'windy', 'e10adc3949ba59abbe56e057f20f883e', '18061581848', 'test@qq.com', 2,
         1);
 INSERT INTO `t_user` (`id`, `username`, `password`, `phone`, `email`, `gender`, `status`)
-VALUES ('4', 'zhengsan', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', '18061581847', 'test@qq.com',
+VALUES ('4', 'zhengsan', 'e10adc3949ba59abbe56e057f20f883e', '18061581847', 'test@qq.com',
         2, 1);
 INSERT INTO `t_user` (`id`, `username`, `password`, `phone`, `email`, `gender`, `status`)
-VALUES ('5', 'lisi', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', '18061581841', 'test@qq.com', 1,
+VALUES ('5', 'lisi', 'e10adc3949ba59abbe56e057f20f883e', '18061581841', 'test@qq.com', 1,
         1);
 INSERT INTO `t_user` (`id`, `username`, `password`, `phone`, `email`, `gender`, `status`)
-VALUES ('6', 'wangwu', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', '18061581842', 'test@qq.com', 1,
+VALUES ('6', 'wangwu', 'e10adc3949ba59abbe56e057f20f883e', '18061581842', 'test@qq.com', 1,
         1);
 INSERT INTO `t_user` (`id`, `username`, `password`, `phone`, `email`, `gender`, `status`)
-VALUES ('7', 'lion', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', '18061581845', 'test@qq.com', 1,
+VALUES ('7', 'lion', 'e10adc3949ba59abbe56e057f20f883e', '18061581845', 'test@qq.com', 1,
         1);
 INSERT INTO `t_user` (`id`, `username`, `password`, `phone`, `email`, `gender`, `status`)
-VALUES ('8', 'shari', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', '18061581844', 'test@qq.com', 1,
+VALUES ('8', 'shari', 'e10adc3949ba59abbe56e057f20f883e', '18061581844', 'test@qq.com', 1,
         1);
 INSERT INTO `t_user` (`id`, `username`, `password`, `phone`, `email`, `gender`, `status`)
-VALUES ('9', 'aewen', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', '18061581843', 'test@qq.com', 1,
+VALUES ('9', 'aewen', 'e10adc3949ba59abbe56e057f20f883e', '18061581843', 'test@qq.com', 1,
         1);
 
