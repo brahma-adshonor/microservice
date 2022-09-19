5f3826868a933ef39381a0f391303d180bc9f35e
tirozhang <tirozhang@tencent.com>
2022-09-19 15:40:57 +0800

    remove  coupon_id for order testdata

--- code/db/db_order/testdata.sql
+++ code/db/db_order/testdata.sql
@@ -1,13 +1,13 @@
 -- ----------------------------
 -- Records of t_order
 -- ----------------------------
-INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (1, '202103011234567890', 0, 2, 0, '202103011234567890', 100, 100, 1, 0, 1, '2021-03-01 12:34:56');
-INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (2, '202103011234567891', 0, 3, 0, '202103011234567891', 100, 100, 1, 0, 2,'2021-03-01 12:34:56');
-INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (3, '202103011234567892', 0, 4, 0, '202103011234567892', 100, 100, 1, 0, 3,'2021-03-01 12:34:56');
-INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (4, '202103011234567893', 0, 2, 0, '202103011234567893', 100, 100, 1, 0, 4,'2021-03-01 12:34:56');
-INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (5, '202103011234567894', 0, 3, 0, '202103011234567894', 100, 100, 1, 0, 5,'2021-03-01 12:34:56');
-INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (6, '202103011234567895', 0, 4, 0, '202103011234567895', 100, 100, 1, 0, 6,'2021-03-01 12:34:56');
-INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (7, '202103011234567896', 0, 2, 0, '202103011234567896', 100, 100, 1, 0, 7,'2021-03-01 12:34:56');
+INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (1, '202103011234567890', 0, 2, 0, '202103011234567890', 100, 100, 0, 0, 1, '2021-03-01 12:34:56');
+INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (2, '202103011234567891', 0, 3, 0, '202103011234567891', 100, 100, 0, 0, 2,'2021-03-01 12:34:56');
+INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (3, '202103011234567892', 0, 4, 0, '202103011234567892', 100, 100, 0, 0, 3,'2021-03-01 12:34:56');
+INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (4, '202103011234567893', 0, 2, 0, '202103011234567893', 100, 100, 0, 0, 4,'2021-03-01 12:34:56');
+INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (5, '202103011234567894', 0, 3, 0, '202103011234567894', 100, 100, 0, 0, 5,'2021-03-01 12:34:56');
+INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (6, '202103011234567895', 0, 4, 0, '202103011234567895', 100, 100, 0, 0, 6,'2021-03-01 12:34:56');
+INSERT INTO `t_order` (`id`,`order_id`, `order_type`, `order_status`, `pay_type`, `trade_no`, `order_amount`, `pay_amount`, `coupon_id`, `coupon_amount`, `user_id`, `pay_at`)VALUES (7, '202103011234567896', 0, 2, 0, '202103011234567896', 100, 100, 0, 0, 7,'2021-03-01 12:34:56');
 
 
 -- ----------------------------
